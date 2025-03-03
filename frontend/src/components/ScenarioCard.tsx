b41690779eebdac738c4bfac126507bd6ae8e21c
Koros <gxgemini777@gmail.com>
2025-03-03 11:02:29 +0800

    dev/修正暗色模式下色彩

--- frontend/src/components/ScenarioCard.tsx
+++ frontend/src/components/ScenarioCard.tsx
@@ -20,9 +20,9 @@ const ScenarioCard: React.FC<ScenarioCardProps> = ({ image, title, description, 
         <figure className="h-fit w-full">
           <img src={image} alt={title} className="object-cover w-full h-full" />
         </figure>
-        <div className="card-body text-center p-4">
-          <h3 className="card-title text-lg font-semibold text-gray-800 w-full text-center block">{title}</h3>
-          <p className="text-sm text-gray-500">{description}</p>
+        <div className="card-body text-center p-4 bg-white">
+          <h3 className="card-title text-lg font-semibold text-gray-800 dark:text-gray-800 w-full text-center block">{title}</h3>
+          <p className="text-sm text-gray-500 dark:text-gray-500">{description}</p>
           <button className="btn btn-info btn-sm mt-2 text-white">選擇場景</button>
         </div>
       </div>
